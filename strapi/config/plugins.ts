631fa5c99f61acc59bd49e2b00821de4a93aba4f
Manoj <manoj_l@techjoomla.com>
2025-04-28 16:01:24 +0530

    Add strapi documentaion plugin and document section config in config -> plugins

--- strapi/config/plugins.ts
+++ strapi/config/plugins.ts
@@ -1,1 +1,33 @@
-export default () => ({});
+// export default () => ({});
+
+module.exports = {
+  documentation: {
+    enabled: true,
+    config: {
+      openapi: '3.0.0',
+      info: {
+        version: '1.0.0',
+        title: 'UBA Provider API Documentation',
+        description: 'Documentation for the UBA Provider Benefits API',
+        contact: {
+          name: 'UBA Provider team',
+          email: 'team@example.com',
+        },
+      },
+      security: [
+        {
+          bearerAuth: []
+        }
+      ],
+      components: {
+        securitySchemes: {
+          bearerAuth: {
+            type: 'http',
+            scheme: 'bearer',
+            bearerFormat: 'JWT',
+          }
+        }
+      }
+    },
+  },
+}; 
